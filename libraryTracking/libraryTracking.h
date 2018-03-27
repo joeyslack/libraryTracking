dcdb6fd13137a973f04ba4ffa5d4443c25684703
Joseph Slack <josephdslack@gmail.com>
2018-03-27 14:55:33 -0700

    Initial Commit

---
+++ libraryTracking/libraryTracking.h
@@ -0,0 +1,19 @@
+//
+//  libraryTracking.h
+//  libraryTracking
+//
+//  Created by Joseph Slack on 3/27/18.
+//  Copyright © 2018 Joseph Slack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libraryTracking.
+FOUNDATION_EXPORT double libraryTrackingVersionNumber;
+
+//! Project version string for libraryTracking.
+FOUNDATION_EXPORT const unsigned char libraryTrackingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libraryTracking/PublicHeader.h>
+
+
